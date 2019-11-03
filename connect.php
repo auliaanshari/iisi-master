114786f1251b3fd576bc8b328f35998d338d6ecd
aafathurr166 <aafathurr166@gmail.com>
2019-11-04 03:02:22 +0700

    db

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "root";
+	$host = "ec2-174-129-253-175.compute-1.amazonaws.com";
+	$user = "rrfwtpwgqleirg";
+	$pass = "9b48502a82909ed8418460ccc2ba71f1b8d755c012fe14ed2487a83568651431";
 	$port = "5432";
-	$dbname = "bkt_tourism";
+	$dbname = "dc9fs5h6senu82";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
